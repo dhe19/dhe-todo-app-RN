c5322bc67be881a903c62bb0f85980645b4c99f3
Edu <info.die@gmail.com>
2023-09-30 18:02:49 -0600

    Update TodoContainer styles

--- src/components/TodoContainer.tsx
+++ src/components/TodoContainer.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./navbar/Navbar";
+import TaskContainer from "./tasks/TaskContainer";
 
 const TodoContainer = () => {
   return (
-    <div className="h-4/5 w-4/5 bg-base-100 rounded-2xl p-8 shadow-xl">
+    <div className="text-[rgb(44, 41, 41)] h-4/5 w-full md:w-4/5 bg-base-100/30 backdrop-blur-md rounded-2xl p-2 py-6 md:p-8 shadow-2xl border-2 border-white/30 overflow-y-scroll no-scrollbar">
       <Navbar />
+      <TaskContainer />
     </div>
   );
 };

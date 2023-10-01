c3947dbdca93a185e383706e2f4215b751a06759
Edu <info.die@gmail.com>
2023-09-30 18:01:39 -0600

    update background

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import TodoContainer from "./components/TodoContainer";
+import Modal from "./components/modal/Modal";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="bg-[#E2E7E9] h-screen flex  items-center justify-center">
+    <div className="bg-[url(./background.jpg)] px-2 md:px-0 bg-cover h-screen flex  items-center justify-center">
       <TodoContainer />
+      <Modal />
     </div>
   );
 }
